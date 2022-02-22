1e0e67dfe32452adfb275ba72bb59a499115d268
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-22 18:55:53 -0300

    Ativando middlewares de erro para toda a aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import JoiError from './controllers/middlewares/JoiError';
+import DomainError from './controllers/middlewares/DomainError';
+import ErrorMiddleware from './controllers/middlewares/Error';
 
 const app = express();
 
 app.use(express.json());
+app.use(JoiError);
+app.use(DomainError);
+app.use(ErrorMiddleware);
 
 export default app;
