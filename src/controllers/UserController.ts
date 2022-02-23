53a4c47739fbaeffb1c9ced71c20c5dff1d8b658
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-23 19:12:32 -0300

    Arrumando validação do Joi e middleware de erro do Joi

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,7 +6,6 @@ import { addService } from '../services/UserService';
 
 export const addController = rescue(async (req: Request, res: Response) => {
   validateSchema(UserSchema, req.body);
-
   const token = await addService(req.body);
 
   res.status(201).json({ token }); 
