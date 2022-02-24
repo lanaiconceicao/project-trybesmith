9d4ef518d489ec93ae4d91931e8662c3bebb0f40
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-24 13:51:00 -0300

    Requisito 2 - Faltando validação de erro

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import JoiError from './controllers/middlewares/JoiError';
 import DomainError from './controllers/middlewares/DomainError';
-import ErrorMiddleware from './controllers/middlewares/Error';
+import ErrorMiddleware from './controllers/middlewares/ErrorMiddleware';
 import { User, Login } from './routes/UsersRouter';
 
 dotenv.config();
@@ -14,7 +14,7 @@ app.use('/users', User);
 app.use('/login', Login);
 
 app.use(JoiError);
-app.use(DomainError);
 app.use(ErrorMiddleware);
+app.use(DomainError);
 
 export default app;
---
+++ src/controllers/middlewares/ErrorMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import ErrorInterface from '../../interfaces/ErrorInterface';
+
+const ErrorMiddleware = (err: ErrorInterface, req: Request, res: Response, _next: NextFunction) => {
+  const { ValidationError } = err;
+  console.log(ValidationError, 'oioioioi');
+  res.status(500).json({
+    message: 'error processing request',
+  });
+};
+
+export default ErrorMiddleware;
