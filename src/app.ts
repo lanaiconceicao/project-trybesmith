02e777b072fa4525709796efefa3f72efef419a8
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-24 12:28:37 -0300

    Criação de rota de login

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 import JoiError from './controllers/middlewares/JoiError';
 import DomainError from './controllers/middlewares/DomainError';
 import ErrorMiddleware from './controllers/middlewares/Error';
-import UserRouter from './routes/UsersRouter';
+import { User, Login } from './routes/UsersRouter';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use('/users', UserRouter);
+app.use('/users', User);
+app.use('/login', Login);
 
 app.use(JoiError);
 app.use(DomainError);
--- src/routes/UsersRouter.ts
+++ src/routes/UsersRouter.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import LoginController from '../controllers/LoginController';
 import { addController } from '../controllers/UserController';
 
-const User = Router();
+export const User = Router();
+export const Login = Router();
 
 // Create user
 User.post('/', addController);
+
+// Login
+Login.post('/', LoginController);
 
 export default User;
