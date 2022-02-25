853292909f8ecb92efa2b4edd713f81992958094
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-25 15:29:32 -0300

    Criação de schema para products com mensagens personalizadas

--- src/controllers/joi/ProductSchema.ts
+++ src/controllers/joi/ProductSchema.ts
@@ -1,8 +1,16 @@
 import Joi from 'joi';
 
-const ProductSchema = Joi.object({
-  name: Joi.string().min(2).required(),
-  amount: Joi.string().min(2).required(),
+export const ProductSchema = Joi.object({
+  name: Joi.string().min(3).required().messages({
+    'string.base': '422|Name must be a string',
+    'string.min': '422|Name must be longer than 2 characters',
+    'any.required': '400|Name is required',
+  }),
+  amount: Joi.string().min(3).required().messages({
+    'string.base': '422|Amount must be a string',
+    'string.min': '422|Amount must be longer than 2 characters',
+    'any.required': '400|Amount is required',
+  }),
 });
 
 export default ProductSchema;
