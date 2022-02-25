4aac249d5c1b0b01c7e27482c7dfbc2eb1fe8d2f
Lanai Conceição <conceicao.lanai@gmail.com>
2022-02-25 15:43:59 -0300

    Model de Product criado

---
+++ src/interfaces/ProductInterface.ts
@@ -0,0 +1,6 @@
+interface ProductInterface {
+  name: 'Espada longa',
+  amount: '30 peças de ouro',
+}
+
+export default ProductInterface;
---
+++ src/models/ProductModel.ts
@@ -0,0 +1,22 @@
+import ProductInterface from '../interfaces/ProductInterface';
+import connection from './connection';
+
+export const addProduct = async (product: ProductInterface) => {
+  const [result] = await connection
+    .query(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);',
+      [product.name, product.amount],
+    );
+  
+  const getId = JSON.parse(JSON.stringify(result));
+
+  // ? não vai passar 'undefined' pra frente
+  return {
+    id: getId[0]?.id,
+    name: product.name,
+    amount: product.amount,
+  };
+};
+
+// retirar:
+export const teste = 0;
